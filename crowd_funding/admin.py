d142c8fcf200fe1e0941791963b7a3fc29f2fb8c
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-12-18 21:17:25 +0100

    Added templates folder

--- crowd_funding/admin.py
+++ crowd_funding/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from crowd_funding.models import *
+admin.site.register(CustomUser)
+admin.site.register(Contribution)
+admin.site.register(Project)
 
-# Register your models here.
+
