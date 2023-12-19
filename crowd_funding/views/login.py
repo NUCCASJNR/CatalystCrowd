387e6b2e47f7724d9b40e911200ed8dd60aac376
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-12-19 17:13:05 +0100

    Added login view

---
+++ crowd_funding/views/login.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""User login view"""
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from crowd_funding.forms.login import User, LoginForm
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+
+
+def login(request):
+    """Login view handler"""
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            try:
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                user = authenticate(username=username, password=password)
+                if user:
+                    auth_login(request, user)
+                    messages.success(request, f'Hi {username.title()}, Welcome Back!')
+                    return redirect('dashboard')
+            except Exception as e:
+                messages.error(request, str(e))
+    else:
+        form = LoginForm()
+    return render(request, 'crowd_funding/login.html', {'form': form})
