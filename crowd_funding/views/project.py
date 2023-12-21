cc1ef3c1a3f375e8ec33e6943de26be3abd1baee
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-12-21 01:24:02 +0100

    Added postview logic

--- crowd_funding/views/project.py
+++ crowd_funding/views/project.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from crowd_funding.forms.project import ProjectForm
 from crowd_funding.models.project import Project
-from .login import login
+from .login import login, dashboard
 
 
 def create_project(request):
@@ -31,3 +31,13 @@ def create_project(request):
                 'end_date': end_date,
                 'project_picture': project_picture
             }
+            project = Project.custom_save(**project_dict)
+            print(project)
+            return redirect(dashboard)
+        else:
+            # Print the form errors
+            print(form.errors)
+    else:
+        form = ProjectForm()
+
+    return render(request, 'crowd_funding/member/add-campaign.html', {'form': form})
