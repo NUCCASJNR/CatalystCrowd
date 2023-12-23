618a305fafc1a89f019834f59aa3d5f9127a746e
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-12-23 21:42:45 +0100

    Worked on project validation

--- crowd_funding/forms/project.py
+++ crowd_funding/forms/project.py
@@ -4,11 +4,40 @@
 
 from django import forms
 from crowd_funding.models.project import Project
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 
 class ProjectForm(forms.ModelForm):
     """Project form"""
+
     class Meta:
         model = Project
         fields = ['category', 'project_name', 'description', 'target_amount', 'start_date',
                   'end_date', 'project_picture']
+
+    def clean_start_date(self):
+        """
+        Validates project start date
+        @return: Validation Error if it is else it returns the date
+        """
+        start_date = self.cleaned_data['start_date']
+        current_datetime = timezone.now()
+
+        if start_date < current_datetime:
+            raise ValidationError("Start date must be the current date or a future date.")
+
+        return start_date
+
+    def clean_end_date(self):
+        """
+        Validates project end date
+        @return:
+        """
+        start_date = self.cleaned_data.get('start_date')
+        end_date = self.cleaned_data.get('end_date')
+
+        if start_date and end_date and end_date <= start_date:
+            raise ValidationError("End date must be after the start date.")
+
+        return end_date
